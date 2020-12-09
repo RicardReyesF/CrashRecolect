cee66f9332cf56335259f789003fee647e5b7e18
Ricardo <rreyesfdz96@gmail.com>
2020-12-08 20:43:43 -0600

    AddProduct

--- lib/src/pages/add_product.dart
+++ lib/src/pages/add_product.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:circular_check_box/circular_check_box.dart';
 
@@ -21,8 +22,11 @@ class _AddProductPageState extends State<AddProductPage> {
              _letras(),
             _letras1(),
             _product(),
+             SizedBox(height: 20.0,),
             _product1(),
+             SizedBox(height: 20.0),
             _product2(),
+             SizedBox(height: 20.0),
             _product3(),
 
            ],
@@ -85,13 +89,13 @@ class _AddProductPageState extends State<AddProductPage> {
           Column(
             children: [
               Container(
-                padding: EdgeInsets.only(left:80.0,top:40.0),
+                padding: EdgeInsets.only(left:60.0,top:40.0),
                 child: Column(
                   children: [
                     Text("Pet",style: TextStyle(fontSize: 15.0,color: Colors.white,fontWeight: FontWeight.w900),),
                     SizedBox(height: 10.0,),
                     GestureDetector(
-                      onTap: (){},
+                      onTap: (){_textfield(context);},
                       child: Image.asset("assets/pet.png",scale: 7.0,),
                     )
                   ],
@@ -100,13 +104,13 @@ class _AddProductPageState extends State<AddProductPage> {
             ],
           ),
           Container(
-              padding: EdgeInsets.only(left:120.0,top:40.0),
+              padding: EdgeInsets.only(left:100.0,top:40.0),
                 child: Column(
                   children: [
                     Text("Vidrio",style: TextStyle(fontSize: 15.0,color: Colors.white,fontWeight: FontWeight.w900),),
                     SizedBox(height: 10.0,),
                     GestureDetector(
-                    onTap: (){},
+                    onTap: (){_textfield(context);},
                     child: Image.asset("assets/vidrio.png",scale: 7.0,),
                     )
                   ]
@@ -121,13 +125,13 @@ class _AddProductPageState extends State<AddProductPage> {
           Column(
             children: [
               Container(
-                padding: EdgeInsets.only(left:80.0,top:40.0),
+                padding: EdgeInsets.only(left:60.0,top:40.0),
                 child: Column(
                   children: [
                     Text("Latas",style: TextStyle(fontSize: 15.0,color: Colors.white,fontWeight: FontWeight.w900),),
                     SizedBox(height: 10.0,),
                     GestureDetector(
-                    onTap: (){},
+                    onTap: (){_textfield(context);},
                     child: Image.asset("assets/lata.png",scale: 7.0,),
                     )
                   ],
@@ -136,13 +140,13 @@ class _AddProductPageState extends State<AddProductPage> {
             ],
           ),
           Container(
-              padding: EdgeInsets.only(left:120.0,top:40.0),
+              padding: EdgeInsets.only(left:100.0,top:40.0),
                 child: Column(
                   children: [
                     Text("Metal",style: TextStyle(fontSize: 15.0,color: Colors.white,fontWeight: FontWeight.w900),),
                     SizedBox(height: 10.0,),
                     GestureDetector(
-                      onTap: (){},
+                      onTap: (){_textfield(context);},
                       child: Image.asset("assets/metal.png",scale: 7.0,),
                     )
                   ]
@@ -157,13 +161,13 @@ class _AddProductPageState extends State<AddProductPage> {
           Column(
             children: [
               Container(
-                padding: EdgeInsets.only(left:80.0,top:40.0),
+                padding: EdgeInsets.only(left:60.0,top:40.0),
                 child: Column(
                   children: [
                     Text("Tapas",style: TextStyle(fontSize: 15.0,color: Colors.white,fontWeight: FontWeight.w900),),
                     SizedBox(height: 10.0,),
                     GestureDetector(
-                      onTap: (){},
+                      onTap: (){_textfield(context);},
                       child: Image.asset("assets/tapa.png",scale: 7.0,),
                     )
                   ],
@@ -172,13 +176,13 @@ class _AddProductPageState extends State<AddProductPage> {
             ],
           ),
           Container(
-              padding: EdgeInsets.only(left:120.0,top:40.0),
+              padding: EdgeInsets.only(left:100.0,top:40.0),
                 child: Column(
                   children: [
                     Text("Cobre",style: TextStyle(fontSize: 15.0,color: Colors.white,fontWeight: FontWeight.w900),),
                     SizedBox(height: 10.0,),
                     GestureDetector(
-                      onTap: (){},
+                      onTap: (){_textfield(context);},
                       child: Image.asset("assets/cobre.png",scale: 7.0,),
                     )
                   ]
@@ -194,13 +198,13 @@ class _AddProductPageState extends State<AddProductPage> {
           Column(
             children: [
               Container(
-                padding: EdgeInsets.only(left:80.0,top:40.0),
+                padding: EdgeInsets.only(left:60.0,top:40.0),
                 child: Column(
                   children: [
                     Text("Baterias",style: TextStyle(fontSize: 15.0,color: Colors.white,fontWeight: FontWeight.w900),),
                     SizedBox(height: 10.0,),
                     GestureDetector(
-                      onTap: (){},
+                      onTap: (){_textfield(context);},
                       child: Image.asset("assets/bateria.png",scale: 7.0,),
                     )
                   ],
@@ -209,19 +213,128 @@ class _AddProductPageState extends State<AddProductPage> {
             ],
           ),
           Container(
-              padding: EdgeInsets.only(left:120.0,top:40.0),
+              padding: EdgeInsets.only(left:100.0,top:40.0),
                 child: Column(
                   children: [
                     Text("Otros",style: TextStyle(fontSize: 15.0,color: Colors.white,fontWeight: FontWeight.w900),),
                     SizedBox(height: 10.0,),
                     GestureDetector(
-                      onTap: (){},
+                      onTap: (){_textother(context);},
                       child: Image.asset("assets/otro.png",scale: 7.0,),
                     )
                   ]
                 )
           ),
         ],
+    );
+  }
+
+  Future _textfield(BuildContext context){
+    return showDialog(
+      context: context,
+      builder: (context) => Align(
+        alignment:Alignment.center,
+        child: Container(
+          padding: EdgeInsets.only(top:20,),
+          width: 300.0,
+          height: 180.0,
+          child: Card(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top:20),
+                  width: 270.0,
+                  height: 75.0,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      counterText: "Precio",
+                      icon: Icon(Icons.monetization_on_rounded),
+                      border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)
+                    ),
+                  ),
+                ),
+              ), 
+               RaisedButton(onPressed: ()=>Navigator.pushNamed(context,'service'),
+                  shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(18.0),
+                  side: BorderSide(color: Colors.blueGrey),
+                  ),
+                  color: Colors.lightBlueAccent,
+                  child: Text('Agregar',style: TextStyle(
+                    color: Colors.blueGrey
+                  ),
+                ),
+              )
+             ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Future _textother(BuildContext context){
+   return showDialog(
+      context: context,
+      builder: (context) => Align(
+        alignment:Alignment.center,
+        child: Container(
+          padding: EdgeInsets.only(top:20,),
+          width: 300.0,
+          height: 250.0,
+          child: Card(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top:20),
+                  width: 270.0,
+                  height: 75.0,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      counterText: "Nombre del producto",
+                      icon: Icon(Icons.article_outlined),
+                      border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.only(top:20),
+                  width: 270.0,
+                  height: 75.0,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      counterText: "Precio",
+                      icon: Icon(Icons.monetization_on_rounded),
+                      border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              
+                RaisedButton(onPressed: ()=>Navigator.pushNamed(context,'service'),
+                  shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(18.0),
+                  side: BorderSide(color: Colors.blueGrey),
+                  ),
+                  color: Colors.lightBlueAccent,
+                  child: Text('Agregar',style: TextStyle(
+                    color: Colors.blueGrey
+                  ),
+                ),
+              )
+             ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
